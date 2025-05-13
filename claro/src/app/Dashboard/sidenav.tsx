d16352e8d4cb631def6c578e21874f096fce5041
Vanessa lins <69607861+Vanessa867@users.noreply.github.com>
2025-05-13 17:38:39 +0000

    ajustes siderbar

--- claro/src/app/Dashboard/sidenav.tsx
+++ claro/src/app/Dashboard/sidenav.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import { useState } from "react";
-import { FaHome, FaUser, FaCogs } from "react-icons/fa";
+import {
+  FaHome,
+  FaClipboardList,
+  FaUserPlus,
+  FaBoxOpen,
+  FaPlusSquare
+} from "react-icons/fa";
 
 const SideNav = () => {
   const [active, setActive] = useState<string>("home");
@@ -10,64 +16,68 @@ const SideNav = () => {
     <div className="bg-white text-gray-900 w-64 h-full p-4 shadow-md">
       <div className="text-xl font-bold mb-6">Ustore</div>
 
-      <ul className="space-y-4">
+      <ul className="space-y-4 text-sm">
         <li>
           <a
             href="#"
             onClick={() => setActive("Dashboard")}
             className={`flex items-center p-2 rounded-md ${
-              active === "Dashboard" ? "bg-blue-100 text-blue-600" : "hover:bg-gray-100"
+              active === "Dashboard" ? "bg-red-100 text-red-600" : "hover:bg-gray-100"
             }`}
           >
             <FaHome className="mr-3" />
             Dashboard
           </a>
         </li>
+
         <li>
           <a
             href="#"
             onClick={() => setActive("ListarPedidos")}
             className={`flex items-center p-2 rounded-md ${
-              active === "ListarPedidos" ? "bg-blue-100 text-blue-600" : "hover:bg-gray-100"
+              active === "ListarPedidos" ? "bg-red-100 text-red-600" : "hover:bg-gray-100"
             }`}
           >
-            <FaUser className="mr-3" />
+            <FaClipboardList className="mr-3" />
             Listar Pedidos
           </a>
         </li>
+
         <li>
           <a
             href="#"
             onClick={() => setActive("CadastrarUsuarios")}
             className={`flex items-center p-2 rounded-md ${
-              active === "CadastrarUsuarios" ? "bg-blue-100 text-blue-600" : "hover:bg-gray-100"
+              active === "CadastrarUsuarios" ? "bg-red-100 text-red-600" : "hover:bg-gray-100"
             }`}
           >
-            <FaCogs className="mr-3" />
+            <FaUserPlus className="mr-3" />
             Cadastrar Usuários
           </a>
         </li>
+
         <li>
           <a
             href="#"
             onClick={() => setActive("CadastrarProdutos")}
             className={`flex items-center p-2 rounded-md ${
-              active === "CadastrarProdutos" ? "bg-blue-100 text-blue-600" : "hover:bg-gray-100"
+              active === "CadastrarProdutos" ? "bg-red-100 text-red-600" : "hover:bg-gray-100"
             }`}
           >
-            <FaUser className="mr-3" />
+            <FaPlusSquare className="mr-3" />
             Cadastrar Produtos
           </a>
         </li>
+
         <li>
           <a
             href="#"
             onClick={() => setActive("ListarProdutos")}
             className={`flex items-center p-2 rounded-md ${
-              active === "ListarProdutos" ? "bg-blue-100 text-blue-600" : "hover:bg-gray-100"
+              active === "ListarProdutos" ? "bg-red-100 text-red-600" : "hover:bg-gray-100"
             }`}
           >
-            <FaUser className="mr-3" />
+            <FaBoxOpen className="mr-3" />
             Listar Produtos
           </a>
         </li>
